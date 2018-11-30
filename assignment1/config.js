2df51aebd2ae19e455d017a0f0f1a7312fff3b5d
megues <marcus@felfel.ch>
2018-12-01 00:00:59 +0100

    Complete first assignment

---
+++ assignment1/config.js
@@ -0,0 +1,18 @@
+
+var environments = {}
+
+environments.staging = {
+  httpPort: 3000,
+  envName: 'staging'
+}
+
+environments.production = {
+  httpPort: 5000,
+  envName: 'production'
+}
+
+var currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : 'staging';
+
+var environmentToReturn = typeof(environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = environmentToReturn;
---
+++ assignment1/index.js
@@ -0,0 +1,59 @@
+var http = require('http');
+var url = require('url')
+var config = require('./config')
+
+
+// Instantiate HTTP server
+const httpServer = http.createServer(function(req, res) {
+  unifiedServer(req, res);
+})
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function() {
+  console.log("Listening on port " + config.httpPort)
+});
+
+// Server logic
+var unifiedServer = function(req, res) {
+  var parsedUrl = url.parse(req.url, true);
+
+  var path = parsedUrl.pathname;
+
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  var method = req.method.toLowerCase();
+
+  var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+  var data = { method }
+
+  chosenHandler(data, function(statusCode, payload) {
+    statusCode = typeof(statusCode) === 'number' ? statusCode : 200
+    payload = typeof(payload) === 'object' ? payload : {};
+
+    var payloadString = JSON.stringify(payload);
+
+    // return the response
+    res.writeHead(statusCode)
+    res.end(payloadString)
+  })
+}
+
+var handlers = {}
+
+handlers.hello = function(data, callback) {
+  if (data.method === 'post') {
+    callback(200, { message: 'Welcome to outer space...'});
+  } else {
+    callback(404)
+  }
+
+}
+
+handlers.notFound = function(data, callback) {
+  callback(404);
+}
+
+var router = {
+  hello: handlers.hello
+}
